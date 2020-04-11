599d068841b13b62da428a8117a721a047590df6
GyubinLee <leegyubin00@gmail.com>
2020-04-11 14:42:53 +0900

    v1.0.2 (apply cors)

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 80;
 app.use(bodyParser.json());
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./build'));
 
